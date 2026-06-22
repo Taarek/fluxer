8e83c3b48edde93f9cb76a230113a09f8372756f
Taarek <13603398+Taarek@users.noreply.github.com>
2026-06-22 22:13:49 +0200

    fix: Prevent app to poll status page on selfhosted
    The app polls in the background for any active incidents or planned
    maintenance, this is not needed for self hosted instances, so now it
    checks this and doesn't run the background pollings if isSelfHosted is
    `true`

--- fluxer_app/src/features/user/state/StatusPage.ts
+++ fluxer_app/src/features/user/state/StatusPage.ts
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
+import RuntimeConfig from '@app/features/app/state/RuntimeConfig';
 
 import RuntimeConfig from '@app/features/app/state/RuntimeConfig';
 import {Logger} from '@app/features/platform/utils/AppLogger';
