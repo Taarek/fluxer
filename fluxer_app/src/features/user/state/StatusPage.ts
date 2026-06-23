87658cbca2f052093707575099be269e6947de4a
Taarek <13603398+Taarek@users.noreply.github.com>
2026-06-23 14:19:32 +0200

    Move the import down a line

--- fluxer_app/src/features/user/state/StatusPage.ts
+++ fluxer_app/src/features/user/state/StatusPage.ts
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-import RuntimeConfig from '@app/features/app/state/RuntimeConfig';
 
+import RuntimeConfig from '@app/features/app/state/RuntimeConfig';
 import {Logger} from '@app/features/platform/utils/AppLogger';
 import {ExternalUrls} from '@fluxer/constants/src/ExternalUrls';
 import {makeAutoObservable, runInAction} from 'mobx';
